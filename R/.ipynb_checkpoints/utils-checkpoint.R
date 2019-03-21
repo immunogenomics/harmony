b18b5e021c405da9345d539d0c4872e6dffcd7c9
Ilya Korsunskiy <ilya.korsunsky@gmail.com>
2019-03-21 09:46:40 -0400

    added reference alignment feature

--- R/.ipynb_checkpoints/utils-checkpoint.R
+++ R/.ipynb_checkpoints/utils-checkpoint.R
@@ -1,215 +1,92 @@
 onehot <- function(x) {
-    data.frame(x) %>% 
-        tibble::rowid_to_column("id") %>% 
-        dplyr::mutate(dummy = 1) %>% 
-        tidyr::spread(x, dummy, fill = 0) %>% 
-        dplyr::select(-id) %>% as.matrix
+  data.frame(x) %>% 
+    tibble::rowid_to_column("id") %>% 
+    dplyr::mutate(dummy = 1) %>% 
+    tidyr::spread(x, dummy, fill = 0) %>% 
+    dplyr::select(-id) %>% as.matrix
 }
 
 
-HarmonyConvergencePlot <- function(harmonyObj) {
-    if (!requireNamespace("ggplot2", quietly = TRUE)) {
-      message("WARNING: Failed to load ggplot2 package. Cannot plot convergence without this package.")
-      return(NA)
-    }
-
-    ## ignore initial value (random initialization)
-    ## break down kmeans objective into rounds
-    obj_fxn <- data.frame(
-        kmeans_idx = Reduce(c, lapply(harmonyObj$kmeans_rounds, function(rounds) {1:rounds})),
-        harmony_idx = Reduce(c, lapply(1:length(harmonyObj$kmeans_rounds), function(i) {rep(i, harmonyObj$kmeans_rounds[i])})),
-        val = tail(harmonyObj$objective_kmeans, -1)
-    ) %>%
-        tibble::rowid_to_column("idx")
-##    data.table(obj_fxn)[, (tail(.SD$val, 1) - head(.SD$val, 1)) / head(.SD$val, 1), by = harmony_idx]
-    obj_fxn %>% ggplot(ggplot2::aes(idx, val, col = harmony_idx)) + 
-      geom_point(shape = 21) + 
-      labs(y = "Objective Function", x = "Iteration Number")
-}
-
-#' Encode vector of values from categorical variable as one-hot matrix 
-#' 
-#' @param
-#' @param
-#' @param
-#' @param
-#' @param
-#' 
-#' @return
-#' 
-#' @export
-#' 
-#' @example
-#' 
-HarmonyMatrix <- function(pc_mat, meta_data, vars_use = NULL, theta = NULL, lambda = NULL,
-                          sigma = 0.1, alpha = .1, nclust = 100, tau = 0, 
-                          block.size = 0.05, max.iter.harmony = 10, 
-                          max.iter.cluster = 200, epsilon.cluster = 1e-5, epsilon.harmony = 1e-4, 
-                          burn.in.time = 10, plot_convergence = FALSE, 
-                          return_object = FALSE, init_mode = "kmeans") {
-
-    ## TODO: check for 
-    ##    partially observed batch variables (WARNING)
-    ##    batch variables with only 1 level (WARNING)
-    ##    if lambda given, check correct length
-    ##    if theta given, check correct length
-    ##    very small batch size and tau=0: suggest tau>0
-    ##    is PCA correct? 
-  
-    if (!'data.frame' %in% class(meta_data)) {
-        if (length(meta_data) %in% dim(pc_mat)) {
-            meta_data <- data.frame(batch_variable = meta_data)
-            vars_use <- 'batch_variable'
-        } else {
-            stop('meta_data must be either a data.frame or a vector with batch values for each cell.')
-        }
-    }
-
-    if (is.null(vars_use)) {
-        stop('Must provides variables to integrate over (e.g. vars_use="stim")')
-    }
-    
-    N <- nrow(meta_data)
-    cells_as_cols <- TRUE
-    if (ncol(pc_mat) != N) {
-        if (nrow(pc_mat) == N) {
-            pc_mat <- t(pc_mat)
-            cells_as_cols <- FALSE
-        } else {
-            stop("ERROR: Number of labels do not correspond to number of samples in PC matrix.")
-        }
-    }
-
-    if (is.null(theta)) {
-        theta <- rep(2, length(vars_use))
-    }
-    if (is.null(lambda)) {
-        lambda <- rep(1, length(vars_use))
-    }    
-    if (length(sigma) == 1 & nclust > 1) {
-        sigma <- rep(sigma, nclust)
-    }
-
-    ## Pre-compute some useful statistics
-    phi <- Reduce(rbind, lapply(vars_use, function(var_use) {t(onehot(meta_data[[var_use]]))}))
-    N_b <- rowSums(phi)
-    Pr_b <- N_b / N
-    B_vec <- Reduce(c, lapply(vars_use, function(var_use) {length(unique(meta_data[[var_use]]))}))
-    theta <- Reduce(c, lapply(1:length(B_vec), function(b) rep(theta[b], B_vec[b])))
-    theta <- theta * (1 - exp(-(N_b / (nclust * tau)) ^ 2))
-
-    lambda <- Reduce(c, lapply(1:length(B_vec), function(b) rep(lambda[b], B_vec[b])))
-    lambda_mat <- diag(c(0, lambda))
-                              
-    ## RUN HARMONY
-    harmonyObj <- new(harmony, 0) ## 0 is a dummy variable - will change later
-    harmonyObj$setup(
-        pc_mat, ## Z
-        phi, ## Phi
-        Pr_b, 
-        sigma, ## sigma
-        theta, ## theta
-        max.iter.cluster, ## max.iter
-        epsilon.cluster, ## kmeans converge.thresh
-        epsilon.harmony, ## harmony epsilon
-        TRUE, ## correct Z_orig only
-        alpha, ## EXPERIMENTAL: alpha, strength of dirichlet prior for DKL penalty
-        nclust, ## K
-        tau, ## tau (desired cells/cluster)
-        block.size, ## model$block.size
-#        rep(1, N), ## EXPERIMENTAL FEATURE: each cell gets its own weight
-        FALSE, ## do linear correction on Z_cos?
-        burn.in.time, ## window size for kmeans convergence
-        lambda_mat
-    )
-
-    ## initialize clusters with kmeans
-    if (init_mode == "kmeans") {
-        harmonyObj$Y <- t(kmeans(t(harmonyObj$Z_cos), centers = nclust, iter.max = 25, nstart = 10)$centers)        
-    } else if (init_mode == "batch_random") {
-        harmonyObj$init_clusters_random_balanced()
-    }
-    harmonyObj$init_cluster()
-
-                               
-
-    harmonyObj$harmonize(max.iter.harmony)
-    if (plot_convergence) plot(HarmonyConvergencePlot(harmonyObj))
-    
-    ## Return either the R6 Harmony object or the corrected PCA matrix (default)
-    if (return_object) {
-      return(harmonyObj)
-    } else {
-      res <- as.matrix(harmonyObj$Z_corr)
-      row.names(res) <- row.names(pc_mat)
-      colnames(res) <- colnames(pc_mat)
-      if (!cells_as_cols) 
-          res <- t(res)
-      return(res)      
-    }
-}
-
-#' Encode vector of values from categorical variable as one-hot matrix 
-#' 
-#' @param
-#' 
-#' @return
-#' 
-#' @export
-#' 
-#' 
-RunHarmony <- function(object, group.by.vars, dims.use, theta = NULL, lambda = NULL, sigma = 0.1, alpha = .1,
-                       nclust = 100, tau = 0, block.size = 0.05, max.iter.harmony = 10, 
-                       max.iter.cluster = 200, epsilon.cluster = 1e-5, epsilon.harmony = 1e-4, 
-                       burn.in.time = 10, plot_convergence = FALSE) {
-    ## CHECK: PCs should be scaled. Unscaled PCs yield misleading results. 
-    ##      sqrt(sum((apply(object@dr$pca@cell.embeddings, 2, sd) - object@dr$pca@sdev) ^ 2))  
-    if (!requireNamespace("Seurat", quietly = TRUE)) {
-      stop("Failed to load Seurat library.")
-    }
-  
-    if (!"seurat" %in% class(object)) {
-        stop("Must pass a Seurat object to RunHarmony function. Did you mean to use the Harmony function?")
-    }    
-    if (!"pca" %in% names(object@dr)) {
-        stop("PCA must be computed before running Harmony.")
-    }
-    if (missing(dims.use)) {
-        dims.use <- 1:ncol(object@dr$pca@cell.embeddings)        
-    } else if (!all(dims.use %in% 1:ncol(object@dr$pca@cell.embeddings))) {
-        stop("Trying to use more dimensions than computed with PCA. Rereun PCA with more dimensions or use fewer PCs.")        
-    }
-    
-    missing.vars <- setdiff(group.by.vars, colnames(object@meta.data))
-    if (length(missing.vars) > 0) {
-      stop(sprintf("ERROR: Primary variable(s) [%s] not in meta.data", paste(missing.vars, collapse = ", ")))
-    }
-  
-      
-    message("Starting harmony")
-    message(sprintf("Using top %d PCs", length(dims.use)))    
-    
-  
-    harmonyEmbed <- HarmonyMatrix(object@dr$pca@cell.embeddings, object@meta.data, group.by.vars, 
-                                   theta, lambda, sigma, alpha, nclust, tau, block.size, max.iter.harmony, 
-                                   max.iter.cluster, epsilon.cluster, epsilon.harmony,
-                                   burn.in.time, plot_convergence, FALSE, 'kmeans')
-      
-  
-    rownames(harmonyEmbed) <- row.names(object@meta.data)
-    colnames(harmonyEmbed) <- paste0("harmony_", 1:ncol(harmonyEmbed))
-    
-    object <- object %>%
-                Seurat::SetDimReduction(reduction.type = "harmony", slot = "cell.embeddings", new.data = harmonyEmbed) %>%
-                Seurat::SetDimReduction(reduction.type = "harmony", slot = "key", new.data = "Harmony") %>%
-                Seurat::ProjectDim(reduction.type = "harmony", replace.dim = T, do.print = F)
-
-    return(object)
-    
+moe_correct_ridge <- function(harmonyObj) {
+  harmonyObj$moe_correct_ridge_cpp()
 }
 
 
+cluster <- function(harmonyObj) {
+  if (harmonyObj$ran_init == FALSE) {
+    stop('Before clustering, run init_cluster')
+  }
+  harmonyObj$cluster_cpp()
+}
 
+harmonize <- function(harmonyObj, iter_harmony, verbose=TRUE) {
+  if (iter_harmony < 1) {
+    return(0)
+  }
+  
+  for (iter in 1:iter_harmony) {
+    if (verbose) {
+        message(sprintf('Harmony %d/%d', iter, iter_harmony))        
+    }
+    
+    # STEP 1: do clustering
+    err_status <- cluster(harmonyObj)
+    if (err_status == -1) {
+      stop('terminated by user')
+    } else if (err_status != 0) {
+      stop(sprintf('Harmony exited with non-zero exit status: %d', err_status))
+    }
+    
+    # STEP 2: regress out covariates
+    moe_correct_ridge(harmonyObj)
+    
+    # STEP 3: check for convergence
+    if (harmonyObj$check_convergence(1)) {
+      if (verbose) {
+          message(sprintf("Harmony converged after %d iterations", iter))    
+      }
+      return(0)
+    }
+  }
+}
+
+
+init_cluster <- function(harmonyObj) {
+  if (harmonyObj$ran_setup == FALSE) {
+    stop('Before initializing cluster, run setup')
+  }
+  
+  harmonyObj$Y <- t(kmeans(t(harmonyObj$Z_cos), centers = harmonyObj$K, iter.max = 25, nstart = 10)$centers)
+  harmonyObj$init_cluster_cpp()
+}
+
+
+HarmonyConvergencePlot <- function(harmonyObj, round_start=1, round_end=Inf, do_wrap=FALSE) {
+  if (!requireNamespace("ggplot2", quietly = TRUE)) {
+    message("WARNING: Failed to load ggplot2 package. Cannot plot convergence without this package.")
+    return(NA)
+  }
+  
+  ## ignore initial value
+  ## break down kmeans objective into rounds
+  obj_fxn <- data.frame(
+    kmeans_idx = Reduce(c, lapply(harmonyObj$kmeans_rounds, function(rounds) {1:rounds})),
+    harmony_idx = Reduce(c, lapply(1:length(harmonyObj$kmeans_rounds), function(i) {rep(i, harmonyObj$kmeans_rounds[i])})),
+    val = tail(harmonyObj$objective_kmeans, -1)
+  ) %>%
+    subset(harmony_idx >= round_start & harmony_idx <= round_end) %>% 
+    tibble::rowid_to_column("idx") 
+  
+  
+  plt <- obj_fxn %>% ggplot(ggplot2::aes(idx, val, col = harmony_idx)) + 
+    geom_point(shape = 21) + 
+    labs(y = "Objective Function", x = "Iteration Number")
+  
+  if (do_wrap) {
+    plt <- plt + facet_grid(.~harmony_idx, scales = 'free', space = 'free_x')
+  } 
+  return(plt)
+}
 
 
 
--- src/.ipynb_checkpoints/harmony-checkpoint.h
+++ src/.ipynb_checkpoints/harmony-checkpoint.h
@@ -5,8 +5,6 @@
 #include <algorithm>
 #include <progress.hpp>
 
-//#include <progress_bar.hpp>
-
 using namespace Rcpp;
 using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
@@ -16,66 +14,50 @@ using namespace std;
 
 class harmony;
 RCPP_EXPOSED_CLASS(harmony)
-
+  
   
 #include "harmony_types.h"
-
+  
 class harmony { 
-  public: 
-    /* CONSTRUCTORS etc */
-    harmony(int __K);    
-    void setup(MATTYPE& __Z, MATTYPE& __Phi, VECTYPE __Pr_b,
-                VECTYPE __sigma, VECTYPE __theta, int __max_iter_kmeans, 
-                float __epsilon_kmeans, float __epsilon_harmony, bool __correct_with_Zorig,
-                float __alpha, int __K, float tau, float __block_size, 
-                //ROWVECTYPE& w_new, 
-                bool __correct_with_cosine,
-                int __window_size, MATTYPE __lambda);
-    
-    /* METHODS */
-    void harmonize(int harmony_iter);
-//    void gmm_correct_armadillo();
-//    void moe_correct_contrast();
-//    void moe_correct_onehot();
-    void init_clusters_random_balanced();
-    void moe_correct_ridge();
-    void init_cluster();
-    int cluster();
-
-    void allocate_buffers();
-    void compute_objective(); 
-    int compute_R(bool random_order);
-    bool check_convergence(int type);
-    
-//    void set_R_merge_flag(float merge_thresh_new);
-//    void update_R_merge();  
-
-    /* FIELDS */
-    MATTYPE R, Z_orig, Z_corr, Z_cos, Y, Phi, Phi_moe; 
-    VECTYPE Pr_b, theta, N_b, sigma, sigma_prior;
-//    ROWVECTYPE w;
-    MATTYPE lambda; // diagonal MATTYPErix of ridge regression penalties
-    vector<float> objective_harmony;
-    vector<float> objective_kmeans, objective_kmeans_dist, objective_kmeans_entropy, objective_kmeans_cross;
-    vector<int> kmeans_rounds; // OLD: Kb
-
-//    vector<uvec> phi_map;
-    float block_size, alpha, epsilon_kmeans, epsilon_harmony, merge_thresh_global;
-    int N, K, B, d, max_iter_kmeans, window_size; 
-    bool correct_with_Zorig, correct_with_cosine;
+public: 
+  /* CONSTRUCTORS etc */
+  harmony(int __K);    
+  void setup(MATTYPE& __Z, MATTYPE& __Phi, MATTYPE& __Phi_moe, VECTYPE __Pr_b,
+             VECTYPE __sigma, VECTYPE __theta, int __max_iter_kmeans, 
+             float __epsilon_kmeans, float __epsilon_harmony, 
+             int __K, float tau, float __block_size, 
+             MATTYPE __lambda, bool __verbose);
   
-    // buffers
-    MATTYPE _scale_dist, dist_mat, O, E, dir_prior, Phi_Rk; // N_k, N_kb, N_b, numerator, denominator, C;
-    uvec update_order, cells_update;
-//    CUBETYPE W;
-//    MATTYPE mu_k, mu_k_r, mu_bk_r,
-//    CUBETYPE mu_bk;
-    MATTYPE W;
-
-    // flags
-    bool ran_setup, ran_init; // do_merge_R;
+  /* METHODS */
+  void moe_correct_ridge_cpp();
+  void init_cluster_cpp();
+  int cluster_cpp();
   
-    // FOR DEBUGGING ONLY - SHOULD ERASE THESE
-//    vector<MATTYPE> R_list;
+  void allocate_buffers();
+  void compute_objective(); 
+  int update_R();
+  bool check_convergence(int type);
+
+  /* FIELDS */
+  MATTYPE R, Z_orig, Z_corr, Z_cos, Y, Y_unnormed, Phi, Phi_moe; 
+  VECTYPE Pr_b, theta, N_b, sigma, sigma_prior;
+  MATTYPE lambda; // diagonal MATTYPErix of ridge regression penalties
+  vector<float> objective_harmony;
+  vector<float> objective_kmeans, objective_kmeans_dist, objective_kmeans_entropy, objective_kmeans_cross;
+  vector<int> kmeans_rounds; // OLD: Kb
+  
+  //    vector<uvec> phi_map;
+  float block_size, epsilon_kmeans, epsilon_harmony, merge_thresh_global;
+  int N, K, B, d, max_iter_kmeans, window_size; 
+
+  // buffers
+  MATTYPE _scale_dist, dist_mat, O, E, dir_prior, Phi_Rk; // N_k, N_kb, N_b, numerator, denominator, C;
+  uvec update_order, cells_update;
+  MATTYPE W;
+  
+  // flags
+  bool ran_setup, ran_init, verbose; // do_merge_R;
+  
 };
 
+  
