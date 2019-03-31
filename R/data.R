5f53f8615b498bc06d8dd420c8dafb128b1a5201
Ilya Korsunsky <ikorsunsky@fvl59.com>
2019-03-31 18:06:30 -0400

    removed large data (ctrl.sparse and stim.sparse)

--- R/data.R
+++ R/data.R
@@ -1,14 +1,17 @@
-#' Sparse gene expression matrix of unstimulated PBMCs.
+#' List of expression matrix, metadata table, and PCA embeddings for 500 stimulated and 500 unstimulated PBMCs. 
 #'
-#' @format A sparse matrix (dgCMatrix) with 13019 cells (columns) and 35635 genes (rows):
+#' @format List of 3 objects: 
+#'   1) exprs_norm_vargenes: dgCMatrix of 1636 genes by 1000 cells. 
+#'   2) meta_data: data.frame
+#'   3) pca_embeddings: matrix of 1000 cells by 20 PCs
 #'
 #' @source \url{https://www.nature.com/articles/nbt.4042}
-"ctrl.sparse"
+"pbmc.small"
 
-#' Sparse gene expression matrix of stimulated PBMCs.
+#' Seurat object with the same data as pbmc.small. 
 #'
-#' @format A sparse matrix (dgCMatrix) with 12875 cells (columns) and 35635 genes (rows):
+#' @format Seurat v2.3.4 object. 
 #'
 #' @source \url{https://www.nature.com/articles/nbt.4042}
-"stim.sparse"
+"pbmc.small.seurat"
 
