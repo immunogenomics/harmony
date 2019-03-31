96be45b7c7f478b3bcf3206a30f3d282bd2528af
Ilya Korsunsky <ikorsunsky@fvl59.com>
2019-03-31 16:17:31 -0400

    removed extra data files. fixed pbmc.small.RData. HarmonyMatrix now takes gene expression matrix as input by default.
    Former-commit-id: 18810077007e52de2c9eaff66ece164ade33203e

--- src/utils.h
+++ src/utils.h
@@ -58,3 +58,49 @@ MATTYPE merge_R(const MATTYPE & R, float thresh = 0.8) {
   }
   return R_new;  
 }
+
+// [[Rcpp::export]]
+MATTYPE scaleRows_dgc(const VECTYPE& x, const VECTYPE& p, const VECTYPE& i, 
+                        int ncol, int nrow, float thresh) {
+    // (0) fill in non-zero elements
+    MATTYPE res = arma::zeros<MATTYPE>(nrow, ncol);
+    for (int c = 0; c < ncol; c++) {
+        for (int j = p[c]; j < p[c + 1]; j++) {
+            res(i[j], c) = x(j);
+        }
+    }
+
+    // (1) compute means
+    VECTYPE mean_vec = arma::zeros<VECTYPE>(nrow);
+    for (int c = 0; c < ncol; c++) {
+        for (int j = p[c]; j < p[c + 1]; j++) {
+            mean_vec(i[j]) += x[j];
+        }
+    }
+    mean_vec /= ncol;
+    
+    // (2) compute SDs
+    VECTYPE sd_vec = arma::zeros<VECTYPE>(nrow);
+    arma::uvec nz = arma::zeros<arma::uvec>(nrow);
+    nz.fill(ncol);
+    for (int c = 0; c < ncol; c++) {
+        for (int j = p[c]; j < p[c + 1]; j++) {
+            sd_vec(i[j]) += (x[j] - mean_vec(i[j])) * (x[j] - mean_vec(i[j])); // (x - mu)^2
+            nz(i[j])--;
+        }
+    }
+        
+    // count for the zeros
+    for (int r = 0; r < nrow; r++) {
+        sd_vec(r) += nz(r) * mean_vec(r) * mean_vec(r);
+    }
+    
+    sd_vec = arma::sqrt(sd_vec / (ncol - 1));
+    
+    // (3) scale values
+    res.each_col() -= mean_vec;
+    res.each_col() /= sd_vec;
+    res.elem(find(res > thresh)).fill(thresh);
+    res.elem(find(res < -thresh)).fill(-thresh);
+    return res;
+}
