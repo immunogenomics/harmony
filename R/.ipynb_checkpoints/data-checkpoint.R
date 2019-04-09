77648478398737c8c19ea674acf622167c939ec4
Ilya Korsunsky <ikorsunsky@fvl59.com>
2019-04-09 18:03:56 -0400

    added celllines data

--- R/.ipynb_checkpoints/data-checkpoint.R
+++ R/.ipynb_checkpoints/data-checkpoint.R
@@ -15,3 +15,12 @@
 #' @source \url{https://www.nature.com/articles/nbt.4042}
 "pbmc.small.seurat"
 
+#' List of metadata table and scaled PCs matrix
+#' 
+#' @format:
+#'   meta_data: data.table of 9478 rows with information about dataset and cell_type
+#'   scaled_pcs: data.table of 9478 rows (cells) and 20 columns (PCs)
+#' 
+#' 
+#' @source \url{https://support.10xgenomics.com/single-cell-gene-expression/datasets}
+"celllines"
