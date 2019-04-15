24ac7928400f212ab651e8ffecc1748b058a4cd9
Ilya Korsunsky <ikorsunsky@fvl59.com>
2019-04-15 19:01:37 -0400

    added vignettes, removed pbmc testing datasets

--- R/data.R
+++ R/data.R
@@ -1,20 +1,3 @@
-#' List of expression matrix, metadata table, and PCA embeddings for 500 stimulated and 500 unstimulated PBMCs. 
-#'
-#' @format List of 3 objects: 
-#'   1) exprs_norm_vargenes: dgCMatrix of 1636 genes by 1000 cells. 
-#'   2) meta_data: data.frame
-#'   3) pca_embeddings: matrix of 1000 cells by 20 PCs
-#'
-#' @source \url{https://www.nature.com/articles/nbt.4042}
-"pbmc.small"
-
-#' Seurat object with the same data as pbmc.small. 
-#'
-#' @format Seurat v2.3.4 object. 
-#'
-#' @source \url{https://www.nature.com/articles/nbt.4042}
-"pbmc.small.seurat"
-
 #' List of metadata table and scaled PCs matrix
 #' 
 #' @format:
