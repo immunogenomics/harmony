93a50d533c14c53e08dba6426f29636e4a23cb35
Ilya Korsunsky <ikorsunsky@fvl59.com>
2019-04-16 09:57:58 -0400

    formatting changes
    Former-commit-id: 5750e03dac5a41c3d6eca404a48cd989b171a268

--- R/harmony-package.r
+++ R/harmony-package.r
@@ -7,5 +7,7 @@
 #' @importFrom Rcpp loadModule
 #' @importFrom methods new
 #' @importFrom methods as
+#' @importFrom rlang .data
+#' @importFrom graphics plot
 loadModule("harmony_module", TRUE)
 NULL
