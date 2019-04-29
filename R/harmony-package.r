25a5e052eb654e0137c4fd5f716a12d550d39b12
Ilya Korsunsky <ikorsunsky@fvl59.com>
2019-04-29 08:35:19 -0400

    removed config.log, added documentation to ?harmony

--- R/harmony-package.r
+++ R/harmony-package.r
@@ -1,4 +1,22 @@
-#' harmony.
+#' Harmony: fast, accurate, and robust single cell integration. 
+#'
+#' Algorithm for single cell integration. 
+#'
+#' @section Usage: 
+#' 
+#' \enumerate{
+#' \item ?HarmonyMatrix to run Harmony on gene expression or PCA 
+#' embeddings matrix.
+#' \item ?RunHarmony to run Harmony on Seurat or SingleCellExperiment objects.
+#' }
+#' @section Useful links:
+#' 
+#' \enumerate{
+#' \item Report bugs at \url{https://github.com/immunogenomics/harmony/issues}
+#' \item Read the manuscript
+#' \href{https://www.biorxiv.org/content/10.1101/461954v2}{online}.
+#' }
+#' 
 #'
 #' @name harmony
 #' @docType package
