4af1a9d3af25748c24772fc165ca4a50e43c9972
Robert Schauner <schaunr@ccf.org>
2021-02-17 15:48:37 -0500

    Add rlang::`%||%` to exports

--- R/harmony-package.r
+++ R/harmony-package.r
@@ -1,22 +1,22 @@
-#' Harmony: fast, accurate, and robust single cell integration. 
+#' Harmony: fast, accurate, and robust single cell integration.
 #'
-#' Algorithm for single cell integration. 
+#' Algorithm for single cell integration.
 #'
-#' @section Usage: 
-#' 
+#' @section Usage:
+#'
 #' \enumerate{
-#' \item ?HarmonyMatrix to run Harmony on gene expression or PCA 
+#' \item ?HarmonyMatrix to run Harmony on gene expression or PCA
 #' embeddings matrix.
 #' \item ?RunHarmony to run Harmony on Seurat or SingleCellExperiment objects.
 #' }
 #' @section Useful links:
-#' 
+#'
 #' \enumerate{
 #' \item Report bugs at \url{https://github.com/immunogenomics/harmony/issues}
 #' \item Read the manuscript
 #' \href{https://www.biorxiv.org/content/10.1101/461954v2}{online}.
 #' }
-#' 
+#'
 #'
 #' @name harmony
 #' @docType package
@@ -28,5 +28,6 @@
 #' @importFrom methods is
 #' @importFrom cowplot plot_grid
 #' @importFrom rlang .data
+#' @importFrom rlang `%||%`
 loadModule("harmony_module", TRUE)
 NULL
