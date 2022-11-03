65e83c83409b72106e42f86f3c9389754977626d
Korsunskiy <ilya.korsunsky@gmail.com>
2022-11-03 16:52:24 -0400

    removed config and pkg_libs from makevars

--- R/data.R
+++ R/data.R
@@ -4,10 +4,10 @@
 #'   meta_data: data.table of 9478 rows with defining dataset and cell_type
 #'   scaled_pcs: data.table of 9478 rows (cells) and 20 columns (PCs)
 #' 
-#' @source \url{support.10xgenomics.com/single-cell-gene-expression/datasets}
+#' @source \url{https://www.10xgenomics.com}
 "cell_lines"
 
 #' Same as cell_lines but smaller (300 cells).
 #' 
-#' @source \url{support.10xgenomics.com/single-cell-gene-expression/datasets}
+#' @source \url{https://www.10xgenomics.com}
 "cell_lines_small"
--- src/harmony_types.h
+++ src/harmony_types.h
@@ -1,16 +1,6 @@
 #include <RcppArmadillo.h>
 
-#if USE_FLOAT_TYPES==1
-typedef arma::fmat MATTYPE;
-typedef arma::fvec VECTYPE;
-typedef arma::frowvec ROWVECTYPE;
-typedef arma::fcube CUBETYPE;
-#endif
-
-#if USE_FLOAT_TYPES==0
 typedef arma::mat MATTYPE;
 typedef arma::vec VECTYPE;
 typedef arma::rowvec ROWVECTYPE;
 typedef arma::cube CUBETYPE;
-#endif
-
