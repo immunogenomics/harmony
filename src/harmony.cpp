72ff8b0068396bd5e122eafda53a9b445e9935c4
Nikos Patikas <nik.patik@gmail.com>
2023-02-14 15:52:34 -0500

    Profiling of the code and fixed bugs that affect the stability of the code
    - Checkpoint for the "correct" version of the algorithm

--- src/harmony.cpp
+++ src/harmony.cpp
@@ -1,11 +1,17 @@
 #include <algorithm>
 #include <chrono>
-#include <Rcpp.h>
 #include "harmony.h"
 #include "types.h"
 #include "utils.h"
 
 
+struct TimerMeta{
+  double elapsed = 0;
+  double last = 0;
+};
+
+
+static std::map<std::string, TimerMeta> timers;
 
 
 
@@ -16,17 +22,21 @@ public:
   Timepoint start;
   std::string task_name;
   double& timer;
-  double initial;
   Timer(std::string _task_name, double& _timer): timer(_timer){
     this->task_name = _task_name;
     this->start = high_resolution_clock::now();
   }
+  Timer(TimerMeta& t) : timer(t.elapsed){
+    t.last = this->timer;
+    this->start = high_resolution_clock::now();
+  }
+  
   double getLapse(){
-    return duration_cast<duration<double>>(high_resolution_clock::now() - this->start).count();
+    return duration_cast<std::chrono::milliseconds>(high_resolution_clock::now() - this->start).count();
   }
   
   ~Timer(){
-    auto time_span = duration_cast<duration<double>>(high_resolution_clock::now() - this->start);
+    auto time_span = duration_cast<std::chrono::milliseconds>(high_resolution_clock::now() - this->start);
     timer += time_span.count();
     // Rcout << "Task: " << this->task_name << " took " << time_span.count() << " seconds" <<std::endl;
   }
@@ -113,7 +123,7 @@ void harmony::init_cluster_cpp(unsigned C) {
   }
 
   // Cosine normalization of columns
-  Y = arma::normalise(Y, 2, 0); 
+  Y = arma::normalise(Y, 2, 0);
   
   dist_mat = 2 * (1 - Y.t() * Z_cos);
   R = dist_mat;
@@ -142,13 +152,13 @@ void harmony::init_cluster_cpp(unsigned C) {
 void harmony::compute_objective() {
   float kmeans_error = as_scalar(accu(R % dist_mat));
   float _entropy = as_scalar(accu(safe_entropy(R).each_col() % sigma)); // NEW: vector sigma
-  float _cross_entropy;
-  _cross_entropy = as_scalar(accu((R.each_col() % sigma) % ((arma::repmat(theta.t(), K, 1) % log((O + 1) / (E + 1))) * Phi)));
+  float _cross_entropy = as_scalar(
+      accu((R.each_col() % sigma) % ((arma::repmat(theta.t(), K, 1) % log((O + 1) / (E + 1))) * Phi)));
 
   // Push back the data
   objective_kmeans.push_back(kmeans_error + _entropy + _cross_entropy);
   objective_kmeans_dist.push_back(kmeans_error);
-  objective_kmeans_entropy.push_back(_entropy); 
+  objective_kmeans_entropy.push_back(_entropy);
   objective_kmeans_cross.push_back(_cross_entropy);
 }
 
@@ -195,48 +205,68 @@ int harmony::cluster_cpp() {
   // R has assumed to not change
   // so update Y to match new integrated data  
   for (iter = 0; iter < max_iter_kmeans; iter++) {
-    // p.increment();
+    
+    {
+      Timer t2(timers["iteration_cluster_cpp"]);
+      // p.increment();
     if (Progress::check_abort())
       return(-1);
     
     // STEP 1: Update Y (cluster centroids)
-    Y = arma::normalise(Z_cos * R.t(), 2, 0);
-    dist_mat = 2 * (1 - Y.t() * Z_cos); // Y was changed
-    Rcout << "Max: " << dist_mat.max() << " Min:" << dist_mat.min() << std::endl;
+    {
+      Timer t(timers["cluster_centroids"]);
+      Y = arma::normalise(Z_cos * R.t(), 2, 0);
+    }
 
-    auto norm = calculate_norm(Y);
-    Rcout << "YMax: " << norm.max() << " YMin:" << norm.min() << ", Size: " << size(norm) << std::endl;
-    
-    auto norm2 = calculate_norm(Z_cos);
-    Rcout << "Z_cosMax: " << norm2.max() << " Z_cosMin:" << norm2.min() << ", Size: " << size(norm2) << std::endl;
-    
+
+    {
+      Timer t(timers["distmat_centroids"]);
+      dist_mat = 2 * (1 - Y.t() * Z_cos); // Y was changed
+    }
+        
     // STEP 3: Update R
-    err_status = update_R();
+    {
+      Timer t(timers["update_R_all"]);
+      err_status = update_R();
+    }
+        
+    
     if (err_status != 0) {
       // Rcout << "Compute R failed. Exiting from clustering." << endl;
       return err_status;
     }
     
     // STEP 4: Check for convergence
-    compute_objective();
+    {
+      Timer t(timers["compute_objective"]);
+      compute_objective();
+    }
+    
     if (iter > window_size) {
       bool convergence_status = check_convergence(0);
       if (convergence_status) {
         //        Rcout << "... Breaking Clustering ..., status = " << convergence_status << endl;
         iter++;
         // Rcout << "Clustered for " << iter << " iterations" << endl;
-        break;        
+        break;
       }
     }
-
+    }
+    Rcout << "Iteration: "<< iter <<
+        " error: " << this->objective_kmeans.back()  <<
+        " dist_error "<< objective_kmeans_dist.back() <<
+        " cros_error "<< objective_kmeans_cross.back() <<
+        " entr_error "<< objective_kmeans_entropy.back() << std::endl;
+        
+    Rcout << "In iteration "<< iter << " " << (timers["iteration_cluster_cpp"].elapsed - timers["iteration_cluster_cpp"].last) / 1000 << std::endl;
+    for(auto& v : timers){
+      Rcout <<"\tTask " << v.first << " took " << v.second.elapsed - v.second.last <<" Overall: " << v.second.elapsed/1000 << std::endl;
+    }
   }
 
   Rcout << std::endl;
   kmeans_rounds.push_back(iter);
   objective_harmony.push_back(objective_kmeans.back());
-  Rcout << "compute_Y(): " << compute_Y_timer<< std::endl;
-  Rcout << "compute_objective(): " << compute_objective_timer<< std::endl;
-  Rcout << "update_R(): " << update_R_timer<< std::endl;
   return 0;
 }
 
@@ -244,37 +274,80 @@ int harmony::cluster_cpp() {
 
 
 int harmony::update_R() {
-  update_order = shuffle(linspace<uvec>(0, N - 1, N));
-  _scale_dist = -dist_mat; // K x N
-  _scale_dist.each_col() /= sigma; // NEW: vector sigma
-  _scale_dist.each_row() -= max(_scale_dist, 0);
-  _scale_dist = exp(_scale_dist);
+  
+  {
+    Timer t(timers["update_R_transform"]);
+    update_order = shuffle(linspace<uvec>(0, N - 1, N));
+    _scale_dist = -dist_mat; // K x N
+    _scale_dist.each_col() /= sigma; // NEW: vector sigma
+    // _scale_dist.each_row() -= max(_scale_dist, 0);
+    _scale_dist = exp(_scale_dist);
+  }
 
   // GENERAL CASE: online updates, in blocks of size (N * block_size)
   unsigned n_blocks = (int)(ceil(1.0 / block_size));
   unsigned cells_per_block = (N / n_blocks) + 1;
-  for (unsigned i = 0; i < n_blocks; i++) {
-    // gather cell updates indices
-    unsigned idx_min = i * cells_per_block;
-    unsigned idx_max = min(idx_min + cells_per_block, N);
-    if (idx_min > idx_max) break;
-    uvec idx_list = linspace<uvec>(idx_min, idx_max - 1, idx_max - idx_min);
-    cells_update = update_order.rows(idx_list);
 
-    // Step 1: remove cells
-    E -= sum(R.cols(cells_update), 1) * Pr_b.t();
-    O -= R.cols(cells_update) * Phi.cols(cells_update).t();
+  // Sort back when done according to this
+  uvec reverse_index = linspace<uvec>(0, N - 1, N);
+  reverse_index = reverse_index(update_order);
 
-    // Step 2: recompute R for removed cells
-    R.cols(cells_update) = _scale_dist.cols(cells_update);
-    R.cols(cells_update) = R.cols(cells_update) % (harmony_pow((E + 1) / (O + 1), theta) * Phi.cols(cells_update));
-    R.cols(cells_update) = normalise(R.cols(cells_update), 1, 0); // L1 norm columns
+  // Allocate new matrices
+  Timer* tp = new Timer(timers["R_update_alloc"]);
+  MATTYPE R_randomized = R.cols(update_order);
+  MATTYPE Phi_randomized = Phi.cols(update_order);
+  MATTYPE Phi_t_randomized = Phi_randomized.t();
+  MATTYPE _scale_dist_randomized = _scale_dist.cols(update_order);
+  delete tp;
+  
+
+
+    for (unsigned i = 0; i < n_blocks; i++) {
+      // gather cell updates indices
+      // unsigned idx_min = i * cells_per_block;
+      // unsigned idx_max = min(idx_min + cells_per_block, N);
+      // if (idx_min > idx_max) break;
+      // uvec idx_list = linspace<uvec>(idx_min, idx_max - 1, idx_max - idx_min);
+      // cells_update = update_order.rows(idx_list);
     
-    // Step 3: put cells back 
-    E += sum(R.cols(cells_update), 1) * Pr_b.t();
-    O += R.cols(cells_update) * Phi.cols(cells_update).t();
+    unsigned idx_max = min((i+1) * cells_per_block, N-1);
+
+    
+    tp = new Timer(timers["R_update_submat"]);
+    auto Rcells = R_randomized.submat(0, i * cells_per_block, R_randomized.n_rows - 1, idx_max);
+    auto Phicells = Phi_randomized.submat(0, i * cells_per_block, Phi_randomized.n_rows - 1, idx_max);
+    auto Phi_tcells = Phi_t_randomized.submat(i * cells_per_block,0, idx_max, Phi_t_randomized.n_cols - 1);
+    auto _scale_distcells = _scale_dist_randomized.submat(0, i * cells_per_block, _scale_dist_randomized.n_rows - 1, idx_max);
+    delete tp;
+
+    
+    {
+      Timer t(timers["R_update_OE_remove"]);
+      // Step 1: remove cells
+      E -= sum(Rcells, 1) * Pr_b.t();
+      O -= Rcells * Phi_tcells;
+    }
+
+
+    {
+      Timer t(timers["R_update_cells"]);
+      // Step 2: recompute R for removed cells
+      Rcells = _scale_distcells;
+      Rcells = Rcells % (harmony_pow((E + 1) / (O + 1), theta) * Phicells);
+      Rcells.each_row() /= sum(Rcells, 0); // normalise(Rcells, 1, 0); // L1 norm columns
+    }
+    {
+      Timer t(timers["R_update_OE_addback"]);
+      // Step 3: put cells back 
+      E += sum(Rcells, 1) * Pr_b.t();
+      O += Rcells * Phi_tcells;
+    }
     
   }
+  
+  this->R = R_randomized.cols(reverse_index);
+  this->Phi = Phi_randomized.cols(reverse_index);
+  
   return 0;
 }
 
--- src/utils.cpp
+++ src/utils.cpp
@@ -26,8 +26,9 @@ MATTYPE safe_entropy(const MATTYPE& X) {
 
 // Overload pow to work on a MATTYPErix and vector
 MATTYPE harmony_pow(MATTYPE A, const VECTYPE& T) {
+
   for (unsigned c = 0; c < A.n_cols; c++) {
-    A.col(c) = pow(A.col(c), as_scalar(T.row(c)));
+    A.unsafe_col(c) = pow(A.unsafe_col(c), as_scalar(T.row(c)));
   }
   return(A);
 }
