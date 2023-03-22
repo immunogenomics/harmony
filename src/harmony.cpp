7db25425933aaeebedb84ffc60df4e4b3d6ad51b
Hongcheng Yao <hongchengyaonk@gmail.com>
2023-03-22 14:26:43 -0400

    change notice message

--- src/harmony.cpp
+++ src/harmony.cpp
@@ -18,7 +18,7 @@ void harmony::setup(MATTYPE& __Z, MATTYPE& __Phi, MATTYPE& __Phi_moe, VECTYPE __
   Z_cos = MATTYPE(Z_orig);
   Z_cos = arma::normalise(Z_cos, 2, 0);
 
-  Rcout << "hcyao change Mar9" << endl;
+  Rcout << "hcyao change Mar22" << endl;
   // cosine_normalize(Z_cos, 0, true); // normalize columns
   
   Phi = __Phi;
