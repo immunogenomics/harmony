6c54ca6e06163c561e5baa3373ec9706541f63e8
Nikos Patikas <nik.patik@gmail.com>
2023-07-21 15:24:44 -0400

    [FIX] convergence plot label

--- R/utils.R
+++ R/utils.R
@@ -71,8 +71,7 @@ HarmonyConvergencePlot <- function(
     plt <- obj_fxn %>% ggplot2::ggplot(ggplot2::aes(.data$idx, .data$val,
                                                     col = as.factor(.data$harmony_idx))) + 
         ggplot2::geom_point(shape = 21) + 
-        ggplot2::labs(y = "Objective Function", x = "Clustering Step #", color = "Integration #") +
-        ggplot2::scale_colour_hue("clarity")
+        ggplot2::labs(y = "Objective Function", x = "Clustering Step #", color = "Integration #")
     
     if (do_wrap) {
         plt <- plt + ggplot2::facet_grid(.~.data$harmony_idx, scales = 'free',
