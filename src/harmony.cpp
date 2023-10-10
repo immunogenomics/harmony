6b290d14423ed605c9d8171ae97ded2499fae89b
Nikos Patikas <nik.patik@gmail.com>
2023-10-10 15:33:03 -0400

    [FIX] update_R bug
    This is a fix of two severe bugs that miscalculates the R for some
    cells:
    
    This procedure was miscalculated in 2 ways:
    
    1) Due to armadillo's submat the last cell's R of each block was
    corrected twice since it was considered as the first cell of next
    block.
    
    2) cells_per_block was rounded down resulting in my_ceil not
    working. As a result the cells_per_block was one less than it
    should. This resulted in up to 1/block_size(by default=0.05), so 19
    cells not being corrected each time update_R is invoked. These cells
    are random so it does not affect the algorithms ability to perform
    corrections it is just adds noise to the gradient descent procedure.

--- src/harmony.cpp
+++ src/harmony.cpp
@@ -235,7 +235,7 @@ int harmony::update_R() {
 
   // GENERAL CASE: online updates, in blocks of size (N * block_size)
   unsigned n_blocks = (int)(my_ceil(1.0 / block_size));
-  unsigned cells_per_block = my_ceil(N / n_blocks);
+  unsigned cells_per_block = my_ceil(N * block_size);
   
   // Allocate new matrices
   MATTYPE R_randomized = R.cols(update_order);
@@ -244,7 +244,7 @@ int harmony::update_R() {
   MATTYPE _scale_dist_randomized = _scale_dist.cols(update_order);
 
   for (unsigned i = 0; i < n_blocks; i++) {
-      unsigned idx_max = min((i+1) * cells_per_block, N-1);
+      unsigned idx_max = min((i+1) * cells_per_block, N) - 1;
       auto Rcells = R_randomized.submat(0, i*cells_per_block, R_randomized.n_rows - 1, idx_max);
       auto Phicells = Phi_randomized.submat(0, i*cells_per_block, Phi_randomized.n_rows - 1, idx_max);
       auto Phi_tcells = Phi_t_randomized.submat(i*cells_per_block, 0, idx_max, Phi_t_randomized.n_cols - 1);
