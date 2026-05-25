2e4afdd340e32ff028ce8560e4229e3e331dddfd
andrewGhazi <6763470+andrewGhazi@users.noreply.github.com>
2026-05-25 17:06:29 -0400

    clickable links, edit vignette command to point to quickstart

--- R/zzz.R
+++ R/zzz.R
@@ -1,21 +1,23 @@
 #' @importFrom utils packageVersion vignette
 .onAttach <- function(libname, pkgname) {
-    ver = packageVersion(pkgname)
-    vignette_built = nrow(vignette(package = 'harmony')$results) != 0
+  ver = packageVersion(pkgname)
+  vignette_built = nrow(vignette(package = 'harmony')$results) != 0
 
-    if (vignette_built) {
-        rebuild_advice = ""
-    } else {
-        rebuild_advice = "Reinstall with {.code build_vignettes = TRUE}, then "
-    }
+  if (vignette_built) {
+    rebuild_advice = ""
+  } else {
+    rebuild_advice = "Reinstall with {.code build_vignettes = TRUE}, then "
+  }
 
-    vign_emph = cli::combine_ansi_styles("bold", "red2")
+  vign_emph = cli::combine_ansi_styles("bold", "red2")
 
-    vignette_msg = paste0('{cli::symbol$bullet} {.strong Read the guide}: ',
-                          rebuild_advice,
-                          'run {vign_emph("vignette()")}')
+  vignette_msg = paste0('{cli::symbol$bullet} {.strong Read the guide}: ',
+                        rebuild_advice,
+                        'run {vign_emph(\'vignette(\"quickstart\", package=\"harmony\")\')}')
 
-    cli::cli_inform(paste0("{cli::symbol$bullet} This is Harmony2 version {.strong ", ver, "}"), class = "packageStartupMessage")
-    cli::cli_inform(vignette_msg, class = "packageStartupMessage")
-    cli::cli_inform("{cli::symbol$bullet} {.strong Get help}: Visit the website at https://korsunskylab.github.io/harmony2/ and report issues on https://github.com/immunogenomics/harmony/issues", class = "packageStartupMessage")
+  cli::cli_inform(paste0("{cli::symbol$bullet} This is Harmony2 version {.strong ", ver, "}"), class = "packageStartupMessage")
+  cli::cli_inform(vignette_msg, class = "packageStartupMessage")
+  cli::cli_inform("{cli::symbol$bullet} {.strong Get help}: Visit the website at {.url https://korsunskylab.github.io/harmony2/} and report issues on {.url https://github.com/immunogenomics/harmony/issues}", class = "packageStartupMessage")
 }
+
+
